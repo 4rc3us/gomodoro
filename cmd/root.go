df94b2d168adf4bbf570e1e7187ad63f155739e1
4rc3us <juandanielarce398@gmail.com>
2024-01-26 17:57:55 -0500

    Short and long description
    Short and long description when the command is invokend

--- cmd/root.go
+++ cmd/root.go
@@ -28,13 +28,9 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "gomodoro",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "gomodoro is just a cli pomodor timer for your terminal",
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long: `CLI timer for your terminal easy and simple to use, just type gomodoro.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },

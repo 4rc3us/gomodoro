cddfc5f13b33f8526e9b7ef5f092302313f35bbb
4rc3us <juandanielarce398@gmail.com>
2024-01-26 18:49:10 -0500

    License in files fix

--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2024 JD arce juandanielarce398@gmail.com
+Copyright © 2024 J Daniel Arce <juandanielarce398@gmail.com>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
